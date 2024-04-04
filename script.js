99ef9fc49ea66b184848cef2189353244842d8e4
atomas95 <tomas.alasauskis@gmail.com>
2024-04-04 18:15:45 +0100

    commit

--- script.js
+++ script.js
@@ -22,5 +22,5 @@ function showSlides(n) {
 
 function toggleMenu() {
   const navbar = document.querySelector('.navbar');
-  navbar.classList.toggle('hidden'); // Use this instead for class-based hiding
+  navbar.classList.toggle('show'); // This will show or hide the navbar
 }
